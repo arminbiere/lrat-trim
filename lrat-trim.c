7ba2eae615771341d436bf3e65bbc76afd66a0e7
Armin Biere <biere@cs.uni-freiburg.de>
2023-01-18 09:10:09 +0100

    complete parsing but not debugged

--- lrat-trim.c
+++ lrat-trim.c
@@ -22,6 +22,7 @@ static const char * usage =
 #include <ctype.h>
 #include <limits.h>
 #include <stdarg.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -41,29 +42,47 @@ struct file input, output;
 
 static int verbosity;
 
-#if 0
-
-struct char_stack {
-  char *begin, *end, *allocated;
-};
-
-#endif
-
 struct int_stack {
-  char *begin, *end, *allocated;
+  int *begin, *end, *allocated;
 };
+
+struct ints_map {
+  int **begin, **end;
+};
+
+#if 0
 
 struct size_map {
   size_t *begin, *end;
 };
+#endif
 
-struct ints_stack {
-  int **begin, **end, **allocated;
+struct deletion {
+  size_t line;
+  int id;
 };
 
+struct deletion_map {
+  struct deletion *begin, *end;
+};
+
+#if 0
+
+struct link {
+  int used, next;
+};
+
+struct link_map {
+  struct link *begin, *end;
+};
+
+#endif
+
 static struct int_stack line;
-static struct ints_stack clauses;
-static struct size_map deleted;
+
+static struct ints_map literals;
+static struct ints_map antecedents;
+static struct deletion_map deleted;
 
 static void die (const char *, ...) __attribute__ ((format (printf, 1, 2)));
 static void err (const char *, ...) __attribute__ ((format (printf, 1, 2)));
@@ -118,6 +137,8 @@ static void vrb (const char *fmt, ...) {
 
 #define SIZE(STACK) ((STACK).end - (STACK).begin)
 #define CAPACITY(STACK) ((STACK).allocated - (STACK).begin)
+
+#define EMPTY(STACK) ((STACK).end == (STACK).begin)
 #define FULL(STACK) ((STACK).end == (STACK).allocated)
 
 #define ENLARGE(STACK) \
@@ -131,28 +152,6 @@ static void vrb (const char *fmt, ...) {
     (STACK).allocated = (STACK).begin + NEW_CAPACITY; \
   } while (0)
 
-#define ADJUST(STACK, NEEDED_SIZE) \
-  do { \
-    size_t OLD_SIZE = SIZE (STACK); \
-    size_t COPY_OF_NEEDED_SIZE = (NEEDED_SIZE); \
-    if (OLD_SIZE < COPY_OF_NEEDED_SIZE) { \
-      size_t OLD_CAPACITY = CAPACITY (STACK); \
-      if (OLD_CAPACITY < COPY_OF_NEEDED_SIZE) { \
-        size_t NEW_CAPACITY = OLD_CAPACITY ? 2 * OLD_CAPACITY : 1; \
-        while (NEW_CAPACITY < COPY_OF_NEEDED_SIZE) \
-          NEW_CAPACITY *= 2; \
-        void *OLD_BEGIN = (STACK).begin; \
-        void *NEW_BEGIN = calloc (NEW_CAPACITY, sizeof *(STACK).begin); \
-        if (!NEW_BEGIN) \
-          die ("out-of-memory adjusting '" #STACK "' stack"); \
-	free (OLD_BEGIN); \
-        (STACK).begin = NEW_BEGIN; \
-        (STACK).allocated = (STACK).begin + NEW_CAPACITY; \
-      } \
-      (STACK).end = (STACK).begin + COPY_OF_NEEDED_SIZE; \
-    } \
-  } while (0)
-
 #define PUSH(STACK, DATA) \
   do { \
     if (FULL (STACK)) \
@@ -160,47 +159,94 @@ static void vrb (const char *fmt, ...) {
     *(STACK).end++ = (DATA); \
   } while (0)
 
+#define CLEAR(STACK) \
+  do { \
+    (STACK).end = (STACK).begin; \
+  } while (0)
+
+#define RELEASE(STACK) free ((STACK).begin)
+
+#define ADJUST(MAP, NEEDED_SIZE) \
+  do { \
+    size_t OLD_SIZE = SIZE (MAP); \
+    size_t COPY_OF_NEEDED_SIZE = (NEEDED_SIZE); \
+    if (OLD_SIZE < COPY_OF_NEEDED_SIZE) { \
+      if (OLD_SIZE < COPY_OF_NEEDED_SIZE) { \
+        size_t NEW_SIZE = OLD_SIZE ? 2 * OLD_SIZE : 1; \
+        while (NEW_SIZE < COPY_OF_NEEDED_SIZE) \
+          NEW_SIZE *= 2; \
+        void *OLD_BEGIN = (MAP).begin; \
+        void *NEW_BEGIN = calloc (NEW_SIZE, sizeof *(MAP).begin); \
+        if (!NEW_BEGIN) \
+          die ("out-of-memory adjusting '" #MAP "' map"); \
+        free (OLD_BEGIN); \
+        (MAP).begin = NEW_BEGIN; \
+        (MAP).end = (MAP).begin + NEW_SIZE; \
+      } \
+      (MAP).end = (MAP).begin + COPY_OF_NEEDED_SIZE; \
+    } \
+  } while (0)
+
+#ifndef NDEBUG
+
+static void release_ints_map (struct ints_map *map) {
+  int **begin = map->begin;
+  int **end = map->end;
+  for (int **p = begin; p != end; p++)
+    if (*p)
+      free (*p);
+  free (begin);
+}
+
+#endif
+
 #ifdef LOGGING
 
-static int logging () { return verbosity == INT_MAX; }
+static bool logging () { return verbosity == INT_MAX; }
 
-static void debug (const char *, ...)
-    __attribute__ ((format (printf, 1, 2)));
-
-static void debug_clause (int *, const char *, ...)
-    __attribute__ ((format (printf, 2, 3)));
-
-static void debug (const char *fmt, ...) {
-  if (!logging ())
-    return;
+static void logging_prefix (const char *fmt, ...) {
+  assert (logging ());
   fputs ("c LOGGING ", stdout);
   va_list ap;
   va_start (ap, fmt);
   vprintf (fmt, ap);
   va_end (ap);
+}
+
+static void logging_suffix () {
+  assert (logging ());
   fputc ('\n', stdout);
   fflush (stdout);
 }
 
-static void debug_clause (int *c, const char *fmt, ...) {
-  assert (c);
-  if (!logging ())
-    return;
-  fputs ("c ", stdout);
-  va_list ap;
-  va_start (ap, fmt);
-  vprintf (fmt, ap);
-  va_end (ap);
-  printf (" clause[%d]", *c);
-  int *p = c + 1, tmp;
-  while ((tmp = *p++))
-    printf (" %d", tmp);
-  fputs (" 0", stdout);
-  while ((tmp = *p++))
-    printf (" %d", tmp);
-  fputs (" 0\n", stdout);
-  fflush (stdout);
-}
+#define dbg(...) \
+  do { \
+    if (!logging ()) \
+      break; \
+    logging_prefix (__VA_ARGS__); \
+    logging_suffix (); \
+  } while (0)
+
+#define dbgs(INTS, ...) \
+  do { \
+    if (!logging ()) \
+      break; \
+    logging_prefix (__VA_ARGS__); \
+    const int *P = (INTS); \
+    while (*P) \
+      printf (" %d", *P++); \
+    logging_suffix (); \
+  } while (0)
+
+#else
+
+#define dbg() \
+  do { \
+  } while (0)
+
+#define dbgs() \
+  do { \
+  } while (0)
 
 #endif
 
@@ -242,6 +288,28 @@ static double mega_bytes (void) {
 }
 
 static double percent (double a, double b) { return b ? a / b : 0; }
+
+static const char *exceeds_int_max (int n, int ch) {
+  static char buffer[32];
+  const size_t size = sizeof buffer - 4;
+  assert (isdigit (ch));
+  sprintf (buffer, "%d", n);
+  size_t i = strlen (buffer);
+  do {
+    assert (i < sizeof buffer);
+    buffer[i++] = ch;
+  } while (i + 1 < size && isdigit (ch = read_char ()));
+  if (ch == '\n') {
+    assert (input.lines);
+    input.lines--;
+    buffer[i++] = '.';
+    buffer[i++] = '.';
+    buffer[i++] = '.';
+  }
+  assert (i < sizeof buffer);
+  buffer[i] = 0;
+  return buffer;
+}
 
 int main (int argc, char **argv) {
 
@@ -295,12 +363,17 @@ int main (int argc, char **argv) {
       err ("expected non-zero digit as first character of line");
     int id = (ch - '0');
     while (isdigit (ch = read_char ())) {
+      assert (id);
       if (INT_MAX / 10 < id)
-        err ("line identifier exceeds 'INT_MAX'");
+      LINE_IDENTIFIER_EXCEEDS_INT_MAX:
+        err ("line identifier '%s' exceeds 'INT_MAX'",
+             exceeds_int_max (id, ch));
       id *= 10;
       int digit = (ch - '0');
-      if (INT_MAX - digit < id)
-        err ("line identifier exceeds 'INT_MAX'");
+      if (INT_MAX - digit < id) {
+        id /= 10;
+        goto LINE_IDENTIFIER_EXCEEDS_INT_MAX;
+      }
       id += digit;
     }
     if (ch != ' ')
@@ -311,79 +384,212 @@ int main (int argc, char **argv) {
     if (ch == 'd') {
       ch = read_char ();
       if (ch != ' ')
-        err ("expected space after 'd'");
+        err ("expected space after '%d d'", id);
       assert (id != INT_MIN);
       PUSH (line, -id);
+      assert (EMPTY (line));
       int last = 0;
-      for (;;) {
+      do {
         ch = read_char ();
         if (!isdigit (ch)) {
           if (last)
-            err ("expected digit after '%d '", last);
+            err ("expected digit after '%d ' "
+                 "in deletion line with identifier %d",
+                 last, id);
           else
-            err ("expected digit after 'd '");
+            err ("expected digit after '%d d ' ", id);
         }
         int other = ch - '0';
         while (isdigit ((ch = read_char ()))) {
           if (!other)
-            err ("unexpected digit '%c' after '0'", ch);
+            err ("unexpected digit '%c' after '0' "
+                 "in deletion line with identifier %d",
+                 ch, id);
           if (INT_MAX / 10 < other)
-            err ("deleted clause identifier exceeds 'INT_MAX'");
+          DELETED_CLAUSE_IDENTIFIER_EXCEEDS_INT_MAX:
+            err ("deleted clause identifier '%s' exceeds 'INT_MAX' "
+                 "in deletion line with identifier %d",
+                 exceeds_int_max (other, ch), id);
           other *= 10;
           int digit = (ch - '0');
-          if (INT_MAX - digit < other)
-            err ("deleted clause identifier exceeds 'INT_MAX'");
+          if (INT_MAX - digit < other) {
+            other /= 10;
+            goto DELETED_CLAUSE_IDENTIFIER_EXCEEDS_INT_MAX;
+          }
           other += digit;
         }
         if (other) {
           if (ch != ' ')
-            err ("expected space after '%d'", other);
+            err ("expected space after '%d' "
+                 "in deletion line with identifier %d",
+                 other, id);
           if (other > id)
             err ("deleted clause identifier '%d' "
-                 "large than line identifier '%d'",
+                 "larger than deletion line identifier '%d'",
                  other, id);
-          if (other >= min_added) {
-            size_t old_size = SIZE (deleted);
-            if ((size_t)other < old_size) {
-              size_t new_size = old_size ? 2 * old_size : 1;
-              while ((size_t)other <= new_size)
-                new_size *= 2;
-              size_t *old_begin = deleted.begin;
-              if (!(deleted.begin = calloc (new_size, sizeof (size_t))))
-                die ("out-of-memory reallocating deleted table");
-              free (old_begin);
-              deleted.end = deleted.begin + new_size;
-            }
-            size_t *deleted_pointer = deleted.begin + other;
-            size_t deleted_before = *deleted_pointer;
-            if (deleted_before)
-              err ("clause[%d] already deleted in line %zu", other,
-                   deleted_before);
-            *deleted_pointer = input.lines + 1;
+          size_t needed_clauses_size = other + 1;
+          ADJUST (deleted, needed_clauses_size);
+          struct deletion *d = deleted.begin + other;
+          if (d->id) {
+            assert (d->line);
+            err ("clause %d requested to be deleted in deletion line "
+                 "with identifier %d was already deleted in deletion "
+                 "line with identifier %d at line %zu",
+                 other, id, d->id, d->line);
           }
-          last = other;
-        } else {
-          if (ch != '\n')
-            err ("expected new-line after '0' at end of deletion line");
-          break;
-        }
-      }
+          size_t deleted_line = input.lines + 1;
+          dbg ("marked clause %d to be deleted at line %zu "
+               "in deletion line with identifier %d",
+               other, deleted_line, id);
+          d->line = deleted_line;
+          d->id = id;
+        } else if (ch != '\n')
+          err ("expected new-line after '0' "
+               "at end of deletion line with identifier %d",
+               id);
+#if !defined(NDEBUG) || defined(LOGGING)
+        PUSH (line, other);
+#endif
+        last = other;
+      } while (last);
+#if !defined(NDEBUG) || defined(LOGGING)
+      dbgs (line.begin, "parsed deletion line with identifier %d "
+                        "and deleted clauses");
+      CLEAR (line);
+#endif
     } else {
       if (id == last_id)
         err ("line identifier '%d' of addition line does not increase", id);
-      line.end = line.begin;
-      while ((ch = read_char ()) != '\n')
-        if (ch == EOF)
-          err ("unexpected end-of-file in clause addition line");
-      // TODO parse literals.
-      // TODO parse antecedents.
-      // TODO copy to actual clause.
-      int * c = 0;
-      ADJUST (clauses, id + 1);
-      clauses.begin[id] = c;
-      debug_clause (c, "added");
+      assert (EMPTY (line));
+      int last = id;
+      assert (last);
+      while (last) {
+        int sign;
+        if ((ch = read_char ()) == '-') {
+          if (!isdigit (ch = read_char ()))
+            err ("expected digit after '%d -' in clause %d", last, id);
+          if (ch == '0')
+            err ("expected non-zero digit after '%d -'", last);
+          sign = -1;
+        } else if (!isdigit (ch))
+          err ("expected literal after '%d ' in clause %d", last, id);
+        else
+          sign = 1;
+        int idx = ch - '0';
+        while (isdigit (ch = read_char ())) {
+          if (!idx)
+            err ("unexpected second '%c' after '%d 0' in clause %d", ch,
+                 last, id);
+          if (INT_MAX / 10 < idx) {
+          VARIABLE_INDEX_EXCEEDS_INT_MAX:
+            if (sign < 0)
+              err ("variable index in literal '-%s' "
+                   "exceeds 'INT_MAX' in clause %d",
+                   exceeds_int_max (idx, ch), id);
+            else
+              err ("variable index '%s' exceeds 'INT_MAX' in clause %d",
+                   exceeds_int_max (idx, ch), id);
+          }
+          idx *= 10;
+          int digit = ch - '0';
+          if (INT_MAX - digit < idx) {
+            idx /= 10;
+            goto VARIABLE_INDEX_EXCEEDS_INT_MAX;
+          }
+          idx += digit;
+        }
+        int lit = sign * idx;
+        if (ch != ' ') {
+          if (idx)
+            err ("expected space after literal '%d' in clause %d", lit, id);
+          else
+            err ("expected space after literals and '0' in clause %d", id);
+        }
+        PUSH (line, lit);
+        last = lit;
+      }
+      size_t needed_clauses_size = id + 1;
+      {
+        size_t size_literals = SIZE (line);
+        size_t bytes_literals = size_literals * sizeof (int);
+        int *l = malloc (bytes_literals);
+        if (!l) {
+          assert (size_literals);
+          err ("out-of-memory allocating literals of size %zu clause %d",
+               size_literals - 1, id);
+        }
+        memcpy (l, line.begin, bytes_literals);
+        ADJUST (literals, needed_clauses_size);
+        literals.begin[id] = l;
+      }
+      CLEAR (line);
+      assert (!last);
+      do {
+        int sign;
+        if ((ch = read_char ()) == '-') {
+          if (!isdigit (ch = read_char ()))
+            err ("expected digit after '%d -' in clause %d", last, id);
+          if (ch == '0')
+            err ("expected non-zero digit after '%d -'", last);
+          sign = -1;
+        } else if (!isdigit (ch))
+          err ("expected clause identifier after '%d ' "
+               "in clause %d",
+               last, id);
+        else
+          sign = 1;
+        int other = ch - '0';
+        while (isdigit (ch = read_char ())) {
+          if (!other)
+            err ("unexpected second '%c' after '%d 0' in clause %d", ch,
+                 last, id);
+          if (INT_MAX / 10 < other) {
+          ANTECEDENT_IDENTIFIER_EXCEEDS_INT_MAX:
+            if (sign < 0)
+              err ("antecedent identifier in '-%s' "
+                   "exceeds 'INT_MAX' in clause %d",
+                   exceeds_int_max (other, ch), id);
+            else
+              err ("antecedent identifier '%s' "
+                   "exceeds 'INT_MAX' in clause %d",
+                   exceeds_int_max (other, ch), id);
+          }
+          other *= 10;
+          int digit = ch - '0';
+          if (INT_MAX - digit < other) {
+            other /= 10;
+            goto ANTECEDENT_IDENTIFIER_EXCEEDS_INT_MAX;
+          }
+          other += digit;
+        }
+        int signed_other = sign * other;
+        if (other && ch != ' ') {
+          err ("expected space after antecedent '%d' in clause %d",
+               signed_other, id);
+          if (!other && ch != '\n')
+            err ("expected new-line after '0' at end of clause %d", id);
+        }
+        PUSH (line, signed_other);
+        last = signed_other;
+      } while (last);
+      {
+        size_t size_antecedents = SIZE (line);
+        size_t bytes_antecedents = size_antecedents * sizeof (int);
+        int *a = malloc (bytes_antecedents);
+        if (!a) {
+          assert (size_antecedents);
+          err ("out-of-memory allocating antecedents of size %zu clause "
+               "%d",
+               size_antecedents - 1, id);
+        }
+        memcpy (a, line.begin, bytes_antecedents);
+        CLEAR (line);
+        ADJUST (antecedents, needed_clauses_size);
+        antecedents.begin[id] = a;
+      }
+      ADJUST (deleted, needed_clauses_size);
       if (!min_added) {
-        debug ("first added clauses clause[%d]", id);
+        vrb ("found first added clause %d", id);
         min_added = id;
       }
     }
@@ -400,7 +606,9 @@ int main (int argc, char **argv) {
   vrb ("parsing finished in %.2f seconds and used %.0f MB", process_time (),
        mega_bytes ());
 
+#ifndef NDEBUG
   free (line.begin);
+#endif
 
   if (output.path) {
     if (!strcmp (output.path, "-")) {
@@ -417,12 +625,11 @@ int main (int argc, char **argv) {
   } else
     msg ("no output file specified");
 
-  for (int **p = clauses.begin; p != clauses.end; p++)
-    if (*p)
-      free (*p);
-  free (clauses.begin);
-
+#ifndef NDEBUG
+  release_ints_map (&literals);
+  release_ints_map (&antecedents);
   free (deleted.begin);
+#endif
 
   msg ("trimmed %9zu addition lines to %9zu lines %3.0f%%", input.added,
        output.added, percent (output.added, input.added));
