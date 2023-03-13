7d64caa616a24afb6c89e47dea9b03fafbf135ea
Armin Biere <biere@cs.uni-freiburg.de>
2023-03-13 11:18:54 +0100

    first two lrit parse errors

--- test/parse/run.sh
+++ test/parse/run.sh
@@ -78,6 +78,39 @@ runlrat () {
   fi
 }
 
+runlrit () {
+  name=$1
+  lrit=$name.lrit
+
+  [ -f $lrit ] || die "can not find '$lrit'"
+
+  log1=$name.log1
+  err1=$name.err1
+
+  $lrattrim $lrit 1>$log1 2>$err1
+  status=$?
+  if [ $status = 1 ]
+  then
+    echo "./lrat-trim test/parse/$lrit # parsing failed as expected"
+  else
+    echo "./lrat-trim test/parse/$lrit # unexpected exit code $status"
+    exit 1
+  fi
+
+  log2=$name.log2
+  err2=$name.err2
+
+  $lrattrim -t -v $lrit 1>$log2 2>$err2
+  status=$?
+  if [ $status = 1 ]
+  then
+    echo "./lrat-trim -t -v test/parse/$lrit # parsing failed as expected"
+  else
+    echo "./lrat-trim -t -v test/parse/$lrit # unexpected exit code $status"
+    exit 1
+  fi
+}
+
 runcnf clausemissing
 runcnf clausesmissing
 runcnf eofincomment1
@@ -145,12 +178,17 @@ runlrat antenonexist
 runlrat antedel
 runlrat antenonl
 
+runlrit eofid1
+runlrit eofid2
+
+lritruns=`grep '^runlrit [a-z]' run.sh|wc -l`
 lratruns=`grep '^runlrat [a-z]' run.sh|wc -l`
 cnfruns=`grep '^runcnf [a-z]' run.sh|wc -l`
-runs=`expr $lratruns + $cnfruns`
+runs=`expr $lritruns + $lratruns + $cnfruns`
 
 cnfs=`ls *.cnf|wc -l`
 lrats=`ls *.lrat|wc -l`
+lrits=`ls *.lrit|wc -l`
 
 log1s=`ls *.log1|wc -l`
 err1s=`ls *.err1|wc -l`
@@ -159,10 +197,11 @@ err2s=`ls *.err2|wc -l`
 
 [ $cnfruns = $cnfs ] || die "found $cnfruns CNF runs in './run.sh' but $cnfs '.cnf' files"
 [ $lratruns = $lrats ] || die "found $lartruns LRAT runs in './run.sh' but $lrats '.lrat' files"
+[ $lritruns = $lrits ] || die "found $lirtruns LRAT runs in './run.sh' but $lrits '.lrit' files"
 
 [ $runs = $log1s ] || die "found $runs runs in './run.sh' but $log1s '.log' files"
 [ $runs = $err1s ] || die "found $runs runs in './run.sh' but $err1s '.err' files"
 [ $runs = $log2s ] || die "found $runs runs in './run.sh' but $log2s '.log' files"
 [ $runs = $err2s ] || die "found $runs runs in './run.sh' but $err2s '.err' files"
 
-echo "passed $runs parsing tests in 'test/parse/run.sh' ($cnfruns CNFs, $lratruns LRATs)"
+echo "passed $runs parsing tests in 'test/parse/run.sh' ($cnfruns CNFs, $lratruns LRATs, $lritruns LRITs)"
