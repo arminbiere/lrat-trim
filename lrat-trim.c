1e9c51c1fed24a792270932ac83cf89a15c05654
Armin Biere <armin.biere@gmail.com>
2025-02-17 18:30:53 +0100

    formatted

--- lrat-trim.c
+++ lrat-trim.c
@@ -86,8 +86,8 @@ static const char * usage =
 #include <limits.h>
 #include <stdarg.h>
 #include <stdbool.h>
-#include <stdio.h>
 #include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
